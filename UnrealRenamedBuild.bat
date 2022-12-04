98b82991e4670177bf8d5e65f173bd0fdec5ae29
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-12-04 19:39:02 +0900

    Update UnrealRenamedBuild.bat

--- UnrealRenamedBuild.bat
+++ UnrealRenamedBuild.bat
@@ -9,18 +9,25 @@ set ProjectName=BuildTest
 @echo off
 set AutomationToolPath=%EngineDirectoryPath%\Binaries\DotNET\AutomationTool.exe
 set UE4ExePath=%EngineDirectoryPath%\Binaries\Win64\UE4Editor-Cmd.exe
+set UProject=%ProjectName%.uproject
 @echo on
 
 :Main
-call :RunUAT "BuildTest_en.uproject" "D:\BuildTest\EN"
+call :RunUAT BuildTest_en "D:\BuildTest\EN"
 pause
 goto EOF
 
 :: .uproject를 복사하고 해당 프로젝트를 이용하여 빌드합니다.
 :: UAT를 이용하여 프로젝트를 빌드하고, 해당 위치에 저장합니다.
 :: UAT가 종료되면 Intermediate폴더를 삭제합니다.
-:RunUAT <SpecificName> <StagingDirectoryPath>
-if not exist %~1 copy %ProjectName%.uproject %~1
-"%AutomationToolPath%" BuildCookRun -project="%ProjectDictionaryPath%\%~1" -noP4 -clientconfig=Shipping -serverconfig=Shipping -nocompile -nocompileeditor -installed -ue4exe="%UE4ExePath%" -utf8output -platform=Win64 -targetplatform=Win64 -build -cook -map= -unversionedcookedcontent -compressed -prereqs -stage -package -stagingdirectory=%~2 -cmdline=" -Messaging"
-if not "%ProjectName%.uproject" == "%~1" del %~1
+:RunUAT <NewProjectName> <StagingDirectoryPath>
+@echo off
+set NewProjectName=%~1
+set NewUProject=%~1.uproject
+set NewUProjectpath=%ProjectDictionaryPath%\%NewUProject%
+set StagingDirectoryPath=%~2
+@echo on
+if not exist %NewUProject% copy %UProject% %NewUProject%
+"%AutomationToolPath%" BuildCookRun -project="%NewUProjectpath%" -noP4 -clientconfig=Shipping -serverconfig=Shipping -nocompile -nocompileeditor -installed -ue4exe="%UE4ExePath%" -utf8output -platform=Win64 -targetplatform=Win64 -build -cook -map= -unversionedcookedcontent -compressed -prereqs -stage -package -stagingdirectory=%StagingDirectoryPath% -cmdline=" -Messaging"
+if not "%ProjectName%.uproject" == "%NewUProject%" del %NewUProject%
 if exist "Intermediate" rd /s /q "Intermediate"
